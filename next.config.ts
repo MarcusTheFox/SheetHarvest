215feb484df58ee0780d1e0d4d76ce205f7f38ff
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2026-04-02 22:41:49 +0300

    try

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/SheetHarvest',
+  trailingSlash: true,
   /* config options here */
 };
 
