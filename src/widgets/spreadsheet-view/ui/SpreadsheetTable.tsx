2f8d1819a95f2565c7d1d78e9c24a8d19deed480
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2026-04-28 14:58:02 +0300

    feat: add tables divider

--- src/widgets/spreadsheet-view/ui/SpreadsheetTable.tsx
+++ src/widgets/spreadsheet-view/ui/SpreadsheetTable.tsx
@@ -6,6 +6,7 @@ import { getCellMergeInfo } from "../lib/merge-utils";
 import { PipelineContext } from "@/features/run-extraction/lib/pipeline/core";
 import { Spinner } from "@heroui/react";
 import { MousePointerClick } from "lucide-react";
+import { Fragment } from "react";
 
 interface SpreadsheetTableProps {
     context?: PipelineContext;
@@ -50,6 +51,11 @@ export const SpreadsheetTable = (props: SpreadsheetTableProps) => {
     }
 
     const rows = context.rows;
+    const tables = rows.reduce((acc, row) => {
+        acc[row.groupIndex] = (acc[row.groupIndex] ?? 0) + 1;
+        return acc;
+    }, [] as number[])
+
     const merges = !context.isColumnStructureModified ? (context.params.merges || []) : [];
 
     const maxCols = rows.reduce((max, row) => Math.max(max, row.cells.length), 0);
@@ -71,34 +77,58 @@ export const SpreadsheetTable = (props: SpreadsheetTableProps) => {
                     </Table.HeaderRow>
                 </Table.Header>
                 <Table.Body>
-                    {rows.map((row, rowIndex) => (
-                        <Table.Row
-                            key={rowIndex}
-                            className={clsx(
-                                "group transition-all",
-                            )}
-                        >
-                            <Table.RowIndexCell>
-                                {row.originalIndex + 1}
-                            </Table.RowIndexCell>
-                            {headers.map((_, idx) => {
-                                const cellValue = row.cells[idx]?.toString() || "";
-                                const { isHidden, rowSpan, colSpan } = getCellMergeInfo(row.originalIndex, idx, merges);
+                    {rows.map((row, rowIndex) => {
+                        const isNewGroup =
+                            rowIndex === 0 || rows[rowIndex - 1].groupIndex !== row.groupIndex;
 
-                                if (isHidden) return null;
+                        return (
+                            <Fragment key={`${row.originalIndex}-${rowIndex}`}>
+                                {isNewGroup && (
+                                    <Table.Row>
+                                        <Table.Cell
+                                            colSpan={headers.length + 1}
+                                            className="py-1! bg-slate-200 border-y border-slate-300 font-bold text-slate-500"
+                                        >
+                                            <div className="flex flex-row justify-between">
+                                                <p>
+                                                    Table {row.groupIndex + 1}
+                                                </p>
+                                                <p>
+                                                    Строк: {tables[row.groupIndex]}
+                                                </p>
+                                            </div>
+                                        </Table.Cell>
+                                    </Table.Row>
+                                )}
+                                <Table.Row
+                                    key={rowIndex}
+                                    className={clsx(
+                                        "group transition-all",
+                                    )}
+                                >
+                                    <Table.RowIndexCell>
+                                        {row.originalIndex + 1}
+                                    </Table.RowIndexCell>
+                                    {headers.map((_, idx) => {
+                                        const cellValue = row.cells[idx]?.toString() || "";
+                                        const { isHidden, rowSpan, colSpan } = getCellMergeInfo(row.originalIndex, idx, merges);
 
-                                return (
-                                    <Table.Cell
-                                        key={`${rowIndex}-${idx}`}
-                                        rowSpan={rowSpan}
-                                        colSpan={colSpan}
-                                    >
-                                        {cellValue}
-                                    </Table.Cell>
-                                );
-                            })}
-                        </Table.Row>
-                    ))}
+                                        if (isHidden) return null;
+
+                                        return (
+                                            <Table.Cell
+                                                key={`${rowIndex}-${idx}`}
+                                                rowSpan={rowSpan}
+                                                colSpan={colSpan}
+                                            >
+                                                {cellValue}
+                                            </Table.Cell>
+                                        );
+                                    })}
+                                </Table.Row>
+                            </Fragment>
+                        )
+                    })}
                 </Table.Body>
             </Table>
         </div>
