44bcddd3cb97c183001ecdf0b72ab65c8ec150bd
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2026-05-05 17:21:34 +0300

    refactor: refactor SpreadsheetStore

--- src/entities/spreadsheet/model/store.ts
+++ src/entities/spreadsheet/model/store.ts
@@ -1,26 +1,15 @@
 import { create } from 'zustand';
-import { SpreadsheetState } from './types';
+import { Sheet } from '@/shared/types/spreadsheet';
+
+export interface SpreadsheetState {
+  sheets: Sheet[];
+  setSheets: (sheets: Sheet[]) => void;
+  reset: () => void;
+}
 
 export const useSpreadsheetStore = create<SpreadsheetState>((set, get) => ({
   sheets: [],
-  currentSheetIndex: 0,
 
-  getCurrentSheet: () => {
-    const { sheets, currentSheetIndex } = get();
-    return sheets.length > 0 ? sheets[currentSheetIndex] : null;
-  },
-
-  setSheets: (sheets) => set({ 
-    sheets, 
-    currentSheetIndex: 0 
-  }),
-
-  setCurrentSheet: (index) => set((state) => ({ 
-    currentSheetIndex: Math.min(Math.max(0, index), state.sheets.length - 1) 
-  })),
-
-  reset: () => set({ 
-    sheets: [], 
-    currentSheetIndex: 0 
-  }),
+  setSheets: (sheets) => set({ sheets }),
+  reset: () => set({ sheets: [] }),
 }));
