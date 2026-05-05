ecfe2534451ee67248412b0a333a260cb9fdab42
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2026-05-05 13:13:39 +0300

    fix: fix data range on parse

--- src/shared/lib/file-parser/xls-legacy-parser.ts
+++ src/shared/lib/file-parser/xls-legacy-parser.ts
@@ -9,9 +9,15 @@ export class XLSLegacyParser implements ISpreadsheetParser {
 
     return workbook.SheetNames.map(name => {
       const sheet = workbook.Sheets[name];
+      if (sheet['!ref']) {
+        const range = XLSX.utils.decode_range(sheet['!ref']);
+        range.s.r = 0;
+        range.s.c = 0;
+        sheet['!ref'] = XLSX.utils.encode_range(range);
+      }
 
       const data = XLSX.utils.sheet_to_json(sheet, { header: 1 }) as TableValue;
-      
+
       const merges: MergeRange[] = (sheet['!merges'] || []).map((merge): MergeRange => ({
         s: { r: merge.s.r, c: merge.s.c },
         e: { r: merge.e.r, c: merge.e.c }
